8abefa1484c8890080584920417d965650ac118f
JamesEagelton <146828989+JamesEagelton@users.noreply.github.com>
2024-01-18 15:58:48 +0000

    Initial commit

---
+++ Assets/cameramover.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class cameramover : MonoBehaviour
+{
+    public float sensX;
+    public Transform orientation;
+    public GameObject cameraholder;
+    public GameObject mainCam;
+    float mouseX;
+    float mouseY;
+    public float sensY;
+    float yRotation;
+    float xRotation;
+
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible= false;
+    }
+
+
+    void Update()
+    {
+        
+        mainCam.transform.position= cameraholder.transform.position;
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90F, 90F);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation  = Quaternion.Euler(0, yRotation, 0);
+
+
+
+    }
+}
